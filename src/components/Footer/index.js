ee03baf42c5af5c744f0731d293c8be4fcd413d9
iqbal125 <iqbal125@yahoo.com>
2020-08-01 20:09:28 -0700

    fix home page

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,10 @@ import styles from './footer.module.css';
 const Footer = () => {
   return (
     <footer>
-      <div className={styles.footer_wrapper}></div>
+      <div className={styles.footer_wrapper}>
+        <div></div>
+        <div></div>
+      </div>
     </footer>
   );
 };
