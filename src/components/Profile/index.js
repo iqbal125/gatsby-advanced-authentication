a038250fa77280bb193efce51f9f39aed08c0cbc
iqbal125 <iqbal125@yahoo.com>
2020-08-02 18:03:39 -0700

    style profile page

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,14 +1,26 @@
 import React, { useContext } from 'react';
 import AuthContext from '../../utils/auth_context';
+import styles from './profile.module.css';
+import { navigate } from 'gatsby';
+import image1 from '../../../static/undraw_balloons_vxx5.svg';
 
 const Profile = () => {
-  const { state } = useContext(AuthContext);
+  const context = useContext(AuthContext);
+
+  const logOut = () => {
+    context.firebase.auth().signOut();
+    context.LogOut();
+    setTimeout(() => navigate('/'), 300);
+  };
 
   return (
-    <div>
-      <h1>Welcome: {state.user.username} </h1>
+    <div className={styles.container}>
+      <h1>Welcome {context.state.user ? context.state.user.username : ''} </h1>
       <h2> This is a private Authenticated Page</h2>
-      <button> Logout</button>
+      <button onClick={logOut} className={styles.logout_button}>
+        Logout
+      </button>
+      <img className={styles.image} src={image1} alt="" />
     </div>
   );
 };
